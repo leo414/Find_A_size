3c74a2236803f9058bdd0218c0b57fb2e055a5a8
zaxlct <zaxlct@foxmail.com>
2016-10-12 00:29:22 +0800

    init

--- README.md
+++ README.md
@@ -1,1 +1,27 @@
-# Find_A_size
+## 1. start
+
+```
+$ npm install
+```
+## 2. dev
+### 2.1 开发调试
+```
+$ npm run dev
+```
+
+查看效果` http://127.0.0.1:8080`
+
+### 2.2 代码检查
+```
+$ npm run lint
+```
+
+### 2.3 测试用例
+```
+$ npm run test:watch
+```
+
+### 2.4 产出资源
+```
+$ npm run build
+```
---
+++ app/index.js
@@ -0,0 +1,71 @@
+import React from 'react'
+import { render } from 'react-dom'
+import { Router, Route, Link, browserHistory } from 'react-router'
+
+const App = React.createClass({
+  render() {
+    return <div>1112</div>
+  }
+})
+const About = React.createClass({
+  render() {
+    return <div>测试</div>
+  }
+})
+const NoMatch = React.createClass({
+  render() {
+    return <div>12</div>
+  }
+})
+
+const Users = React.createClass({
+  render() {
+    return (
+      <div>
+        <h1>Users</h1>
+        <div className="master">
+          <ul>
+            {/* use Link to route around the app */}
+            {this.state.users.map(user => (
+              <li key={user.id}><Link to={`/user/${user.id}`}>{user.name}</Link></li>
+            ))}
+          </ul>
+        </div>
+        <div className="detail">
+          {this.props.children}
+        </div>
+      </div>
+    )
+  }
+})
+
+const User = React.createClass({
+  componentDidMount() {
+    this.setState({
+      // route components are rendered with useful information, like URL params
+      user: findUserById(this.props.params.userId)
+    })
+  },
+
+  render() {
+    return (
+      <div>
+        <h2>{this.state.user.name}</h2>
+        {/* etc. */}
+      </div>
+    )
+  }
+})
+
+
+render((
+  <Router history={browserHistory}>
+    <Route path="/" component={App}>
+      <Route path="about" component={About}/>
+      <Route path="users" component={Users}>
+        <Route path="/user/:userId" component={User}/>
+      </Route>
+      <Route path="*" component={NoMatch}/>
+    </Route>
+  </Router>
+), document.getElementById('app'))
