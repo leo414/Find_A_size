6adc281b9ba41260fbc2c87dd3ddbef5b90f2ded
zaxlct <zaxlct@foxmail.com>
2016-10-12 20:55:43 +0800

    init

---
+++ app/components/App.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import Nav from './Layout/Nav'
+
+const App = () => (
+  <div>
+    <Nav />
+  </div>
+)
+
+export default App
---
+++ app/components/Layout/Nav.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Nav = () => (
+  <nav>
+    我是导航
+  </nav>
+)
+
+export default Nav
