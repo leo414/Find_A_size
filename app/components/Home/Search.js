3cee7478c5bf4bca4bd00b244a8c712a01cb47c5
zaxlct <zaxlct@foxmail.com>
2016-10-13 20:53:02 +0800

    search component

--- app/components/Home/Search.js
+++ app/components/Home/Search.js
@@ -1,11 +1,17 @@
 import React from 'react'
 
 const Search = () => (
-  <div className="container">
-    <h1>Looking for a furniture? Never by full priced furnitures!</h1>
-    <h5>Watch the price drop and save on the best price ever!</h5>
-    <input type="text"/><i className="sousuo" />
-    <small>Example: industrial coffee table</small>
+  <div className="search container">
+    <section className="title">
+      <h1>Looking for a furniture? Never by full priced furnitures!</h1>
+      <p>Watch the price drop and save on the best price ever!</p>
+    </section>
+
+    <div className="search">
+      <input className="in_block" type="text" placeholder="Search" />
+      <span className="search_icon in_block"><i className="sousuo" /></span>
+    </div>
+    <small className="small">Example: industrial coffee table</small>
   </div>
 )
 
