f2dbc74edeb2d234e9f98caa44bd8008845108a7
zaxlct <zaxlct@foxmail.com>
2016-10-13 20:17:23 +0800

    导航布局

--- app/components/Layout/Nav.js
+++ app/components/Layout/Nav.js
@@ -1,14 +1,22 @@
 import React from 'react'
+import {Link} from 'react-router'
 
 const Nav = () => (
   <nav className="nav">
     <div className="container">
-      <span className="logo">Find-A-Size</span>
-      <input type="text"/>
-      <i className="sousuo-small" />
-      <div className="entry">Log in | Sign up</div>
-      <i className="ring" />
-      <div className="my_list">My List</div>
+      <span className="logo in_block">Find-A-Size</span>
+      <div className="search in_block">
+        <input type="text"/>
+        <i className="sousuo-small" />
+      </div>
+
+      <div className="entry in_block">
+        <Link href="#">Log in</Link>
+        &nbsp;|&nbsp;
+        <Link href="#">Sign up</Link>
+      </div>
+
+      <div className="my_list fr"><i className="ring" />&nbsp;My List</div>
     </div>
   </nav>
 )
