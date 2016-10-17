4eaff88b575d53c6d91a9fb0ed6edf1678bd8781
zaxlct <zaxlct@foxmail.com>
2016-10-17 12:25:23 +0800

    login page

--- app/components/Home/index.js
+++ app/components/Home/index.js
@@ -3,24 +3,42 @@ import Nav from '../Layout/Nav'
 import Footer from '../Layout/Footer'
 import ProductList from '../Layout/ProductList'
 
+import Mask from '../Common/Mask'
+
+import Login from '../Entry/Login'
+
 import Search from '../Search'
 import EmailSignUp from './EmailSignUp'
 
-const Home = () => (
-  <div>
-    <Nav />
-    <Search />
+class Home extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      open: false,
+    }
+  }
+  render() {
+    return (
+      <div>
+        <Mask open={this.state.open} />
+        <Nav />
+        <Search />
 
-    <hr style={{height: '1px', border: 'none', borderTop: '1px solid #ccc', margin: '50px 0'}} />
+        <Login />
 
-    <ProductList type="A">Popular Products</ProductList>
-    <ProductList type="B">You may interetsed in these…</ProductList>
+        <hr style={{height: '1px', border: 'none', borderTop: '1px solid #ccc', margin: '50px 0'}} />
 
-    <hr style={{height: '1px', border: 'none', borderTop: '1px solid #ccc', margin: '50px 0'}} />
+        <ProductList type="A">Popular Products</ProductList>
+        <ProductList type="B">You may interetsed in these…</ProductList>
 
-    <EmailSignUp />
-    <Footer />
-  </div>
-)
+        <hr style={{height: '1px', border: 'none', borderTop: '1px solid #ccc', margin: '50px 0'}} />
+
+        <EmailSignUp />
+        <Footer />
+      </div>
+    )
+  }
+
+}
 
 export default Home
