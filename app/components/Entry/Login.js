3944eb3520e845db9827f0ba574a27b496a9e2a1
zaxlct <zaxlct@foxmail.com>
2016-10-17 19:00:03 +0800

    sign up page

--- app/components/Entry/Login.js
+++ app/components/Entry/Login.js
@@ -1,58 +1,54 @@
 import React from 'react'
-
 import Button from '../Common/Button'
 
 const Login = () => {
   return (
-    <div>
-      <div id="mask"></div>
-      <section className="login_page">
-        <p className="h1 color_green">WELCOME BACK!</p>
+    <section className="login_page hidden">
+      <p className="h1 color_green">WELCOME BACK!</p>
 
-        <Button
-          width="320px"
-          height="44px"
-          fontSize="22px"
-          className="red"
-          handleSubmit={() => onSignup()}
-          value="Log in with Google"
-        />
+      <Button
+        width="320px"
+        height="44px"
+        fontSize="22px"
+        className="red"
+        handleSubmit={() => onSignup()}
+        value="Log in with Google"
+      />
 
-        <br/>
-        <br/>
+      <br/>
+      <br/>
 
-        <Button
-          width="320px"
-          height="44px"
-          fontSize="22px"
-          className="deongaree"
-          handleSubmit={() => onSignup()}
-          value="Log in with Facebook"
-        />
+      <Button
+        width="320px"
+        height="44px"
+        fontSize="22px"
+        className="deongaree"
+        handleSubmit={() => onSignup()}
+        value="Log in with Facebook"
+      />
 
-        <p className="subtitle">Don’t have an account? &nbsp;&nbsp;&nbsp;&nbsp;<a><strong className="color_green">SIGN UP</strong></a></p>
+      <p className="subtitle">Don’t have an account? &nbsp;&nbsp;&nbsp;&nbsp;<a><strong className="color_green">SIGN UP</strong></a></p>
 
-        <hr style={{height: '1px', border: 'none', borderTop: '1px solid #eee', marginTop: '40px'}} />
-        <p className="h1 color_green or">OR</p>
+      <hr style={{height: '1px', border: 'none', borderTop: '1px solid #eee', marginTop: '40px'}} />
+      <p className="h1 color_green or">OR</p>
 
-        <form>
-          <input type="text" placeholder="Username" /> <br/>
-          <input type="email" placeholder="Email Address" />  <br/>
-          <input type="password" placeholder="Password" />
-        </form>
+      <form>
+        <input type="text" placeholder="Username" /> <br/>
+        <input type="email" placeholder="Email Address" />  <br/>
+        <input type="password" placeholder="Password" />
+      </form>
 
-        <Button
-          width="250px"
-          height="38px"
-          fontSize="18px"
-          className="green"
-          handleSubmit={() => onSignup()}
-          value="LOG IN"
-        />
+      <Button
+        width="250px"
+        height="38px"
+        fontSize="18px"
+        className="green"
+        handleSubmit={() => onSignup()}
+        value="LOG IN"
+      />
 
-        <p className="subtitle"><a>Forgot your password?</a></p>
-      </section>
-    </div>
+      <p className="subtitle"><a>Forgot your password?</a></p>
+    </section>
   )
 }
 
