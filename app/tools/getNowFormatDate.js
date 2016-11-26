6392e587f56903b5206e9ce8700d8d58425238c7
zaxlct <zaxlct@foxmail.com>
2016-11-26 10:11:18 +0800

    fix when minutes < 10

--- app/tools/getNowFormatDate.js
+++ app/tools/getNowFormatDate.js
@@ -4,14 +4,19 @@ function getNowFormatDate() {
   var seperator2 = ":";
   var month = date.getMonth() + 1;
   var strDate = date.getDate();
+  var minutes = date.getMinutes()
   if (month >= 1 && month <= 9) {
     month = "0" + month;
   }
   if (strDate >= 0 && strDate <= 9) {
     strDate = "0" + strDate;
   }
+  if (minutes >= 0 && minutes <= 9) {
+    minutes = "0" + minutes;
+  }
+
   var currentdate = date.getFullYear() + seperator1 + month + seperator1 + strDate +
-    " " + date.getHours() + seperator2 + date.getMinutes() +
+    " " + date.getHours() + seperator2 + minutes +
     seperator2 + date.getSeconds();
   return currentdate;
 }
