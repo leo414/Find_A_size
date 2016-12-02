14925f45b1194506a7f518ffa87d7e37cc2cd9ff
zaxlct <zaxlct@foxmail.com>
2016-12-02 22:07:03 +0800

    SearchInput Container

--- app/components/Search/SearchFilter.js
+++ app/components/Search/SearchFilter.js
@@ -29,6 +29,19 @@ class SearchFilter extends React.Component {
   render() {
     return (
       <article>
+        <section className="search_input container">
+          <div className="title">
+            <h1>Looking for a furniture? Never by full priced furnitures!</h1>
+            <p>Watch the price drop and save on the best price ever!</p>
+          </div>
+
+          <div className="search">
+            <input className="in_block" type="text" placeholder="Search" />
+            <span className="search_icon in_block"><i className="sousuo" /></span>
+          </div>
+          <small className="small">Example: industrial coffee table</small>
+        </section>
+
         <section className="search_filter container">
           <div className="fl">
             <h3><em>Refine by:</em></h3>
